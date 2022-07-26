6ae936e13c29187c26e593cf2384f01f0224a939
Ray <wakeup@asleep.ai>
2022-07-26 17:56:42 +0900

    Clean up the code

--- one_cycle/one_cycle.py
+++ one_cycle/one_cycle.py
@@ -1,83 +1,48 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
 import dataset
 
-training_data = dataset.SleepSoundDataset(
-    root="data",
-    train=True
-)
 
-test_data = dataset.SleepSoundDataset(
-    root="data",
-    train=False
-)
-
-batch_size = 8
-
-# 데이터로더를 생성합니다.
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
-for X, y in test_dataloader:
-    print(f"Shape of X [batch_size, num_feature]: {X.shape}")
-    print(f"Shape of y: {y.shape} {y.dtype}")
-    break
-
-
-# Get resources to run the experiment.
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
-
-# define the model
 class NeuralNetwork(nn.Module):
-    """
+    """Define the neural network model.
     """
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.flatten = nn.Flatten()
+        model_size = 128
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(20, 32),
+            nn.Linear(20, model_size),
             nn.ReLU(),
-            nn.Linear(32, 32),
+            nn.Linear(model_size, model_size),
             nn.ReLU(),
-            nn.Linear(32, 2)
+            nn.Linear(model_size, 2)
         )
 
     def forward(self, x):
+        """The forward pass of the model.
+        """
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
 
-model = NeuralNetwork().to(device)
-print(model)
-
-
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
 
 def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    for batch, (X, y) in enumerate(dataloader):
+    for (X, y) in dataloader:
         X, y = X.to(device), y.to(device)
-        # y = y.float()
 
-        # 예측 오류 계산
+        # forward pass
         pred = model(X)
-        # print(pred, y)
+
+        # compute loss
         loss = loss_fn(pred, y)
-        # print(loss)
-        # 역전파
+
+        # Back propagation
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        if batch % 8 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+    return loss.item()
 
 
 def test(dataloader, model, loss_fn):
@@ -89,20 +54,43 @@ def test(dataloader, model, loss_fn):
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
-            print(pred, y, pred.argmax(1), pred.argmax(1) == y)
+            # print(pred, y, pred.argmax(1), pred.argmax(1) == y)
 
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"[Test ] Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}")
 
 
+if __name__ == '__main__':
+    batch_size = 8
 
-epochs = 10000
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    if t % 50 == 0:
-        test(test_dataloader, model, loss_fn)
-print("Done!")
+    # Create the dataset for training and testing
+    training_data = dataset.SleepSoundDataset(root="data", train=True)
+    test_data = dataset.SleepSoundDataset(root="data", train=False)
+
+    # Create a DataLoader for the training and test data
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    # Get resources to run the experiment.
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device} device")
+
+    # Create the model
+    model = NeuralNetwork().to(device)
+
+    # Create the loss function and the optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    # Train the model
+    num_epochs = 10000
+    for epoch in range(num_epochs):
+        train_loss = train(train_dataloader, model, loss_fn, optimizer)
+        if epoch % 200 == 0:
+            print(f"Epoch {epoch} -------------------------------")
+            print(f"[Train] Loss: {train_loss:>7f}")
+            test(test_dataloader, model, loss_fn)
+    print("Done!")
